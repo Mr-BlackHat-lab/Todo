ee76dca123830cff8a0267a6c5b0ac577496a1c7
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2024-12-24 08:51:25 +0530

    picking where left bt using new method vites

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "./myocomponents/todo.jsx"
+
+function App() {
+  return (
+    <>
+    <Todo/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/myocomponents/todo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../cssOfComponents/todo.css"
+export default function Todos(props){
+    return(
+        <div className="container">
+            <div className="todo-app">
+                <h2>To-Do List <img src="/images/icon.png" /></h2>
+            </div>
+
+        </div>
+    )
+}
