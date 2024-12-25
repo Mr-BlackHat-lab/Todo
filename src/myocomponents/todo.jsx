1520c3dc65c16da75b301e60ff1777760613fb6b
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2024-12-25 11:35:59 +0530

    added input box and button

--- src/myocomponents/todo.jsx
+++ src/myocomponents/todo.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import "../cssOfComponents/todo.css"
-export default function Todos(props){
-    return(
-        <div className="container">
-            <div className="todo-app">
-                <h2>To-Do List <img src="/images/icon.png" /></h2>
-            </div>
-
+import "../cssOfComponents/todo.css";
+export default function Todos(props) {
+  return (
+    <div className="container">
+      <div className="todo-app">
+        <h2>
+          To-Do List <img src="/images/icon.png" />
+        </h2>
+        <div className="row">
+          <input type="text" id="Input-box" placeholder="Add you task" />
+          <button>Add</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
